174ac8ffec137e4a9896d26769c7cc8e7647ce8c
justintran12 <jttranjustin@gmail.com>
2023-05-08 18:35:21 -0700

    merged get season data with get career data function

--- baseball_stats_server.py
+++ baseball_stats_server.py
@@ -5,9 +5,14 @@ import statsapi
 app = Flask(__name__)
 CORS(app)
 
-def getCareerDataToMap(player_name):
-	player_stats_str = statsapi.player_stats(next(x['id'] for x in statsapi.get('sports_players',{'season':2022,'gameType':'W'})['people'] if x['fullName']== player_name), 'hitting', 'career')
-	player_stats_arr = player_stats_str.split("Career Hitting")[1].strip().split("\n")
+# convert player data frpm api call into map
+def getPlayerHittingStatsToMap(player_name, season_type):
+	if season_type == 'career':
+		player_stats_str = statsapi.player_stats(next(x['id'] for x in statsapi.get('sports_players',{'season':2022,'gameType':'W'})['people'] if x['fullName']== player_name), 'hitting', 'career')
+		player_stats_arr = player_stats_str.split("Career Hitting")[1].strip().split("\n")
+	else: # season
+		player_stats_str = statsapi.player_stats(next(x['id'] for x in statsapi.get('sports_players',{'season':2023,'gameType':'W'})['people'] if x['fullName']== player_name), 'hitting', 'season')
+		player_stats_arr = player_stats_str.split("Season Hitting")[1].strip().split("\n")
 
 	player_stats_map = {}
 	for stat in player_stats_arr:
@@ -21,7 +26,7 @@ def getCareerDataToMap(player_name):
 @app.route('/career', methods = ['GET'])
 def getDataCareer():
 	player = "Jarred Kelenic"
-	player_stats_map = getCareerDataToMap(player)
+	player_stats_map = getPlayerHittingStatsToMap(player, 'career')
 	
 	'''
 	@after_this_request
@@ -35,6 +40,9 @@ def getDataCareer():
 @app.route('/seasons', methods = ['GET'])
 def getDataSeasons():
 	player = "Jarred Kelenic"
+	player_stats_map = getPlayerHittingStatsToMap(player, 'season')
+	'''
+	# another way to get player hitting stats for season but only works if you know player id
 	#player_id = statsapi.lookup_player(player)[0]['id']
 	player_id = 672284
 	player_info = statsapi.player_stat_data(player_id, group="[hitting,fielding]", type="season", sportId=1)
@@ -45,9 +53,10 @@ def getDataSeasons():
 	for stat in player_stats:
 		if (stat["group"] == 'hitting'):
 			hitting_stats = stat["stats"]
+	'''
 		
 		
-	return jsonify(hitting_stats)
+	return jsonify(player_stats_map)
 
 if __name__ == '__main__':
    app.run(debug = True)
